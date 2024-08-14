75a0c7f82feb2b749d6ec395c3fb0ba59f7b7212
BlackKing72 <matheus.csilv14@gmail.com>
2024-08-14 19:11:54 -0300

    Iniciando o layout da lista de postagens

--- src/pages/PostViewPage.jsx
+++ src/pages/PostViewPage.jsx
@@ -1,7 +1,10 @@
 import MDEditor from '@uiw/react-md-editor';
 import * as postService from '../services/postService'
 
-const getResumeFromContent = (content, useCompact, maxLength) => {
+import './PostViewPage.css'
+import PostCard from '../components/PostCard';
+
+const getResumeFromContent = (content, useCompact, includeTitles, maxLength) => {
     const removeImagePattern = /!\[.*\](.*)/g;
     const removeHeaderPattern = /^#{1,6} [a-zA-Z ]*\n/gm;
     const removeDividerPattern = /^\_{3}\n/gm;
@@ -9,12 +12,14 @@ const getResumeFromContent = (content, useCompact, maxLength) => {
     const removeBlockQuotesPattern = /^>.*\n/gm;
     const removeListPattern = /- .*\n/gm;
 
+    if (includeTitles) {
+        const titlePattern = /^[#]{1,6}\s+(.*\n)/gm;
+        content = content.replace(titlePattern, '$1');
+    }
+    
     const textOnlyPattern = /^[a-zA-Z#].*\n/gm;
-    const matches = content
-        .replace(/^[#]{1,6}\s+(.*\n)/gm, '$1')
-        .matchAll(textOnlyPattern);
+    const matches = content.matchAll(textOnlyPattern);
 
-    /** @type {String} */
     let resume = '';
     for (const match of matches) {
         if (match.index !== 0 && !useCompact)
@@ -47,10 +52,7 @@ const PostViewList = ({posts}) => {
         <div>
         {
             posts.map(post => 
-                <div className=''>
-                    <h1>{post.title}</h1>
-                    <MDEditor.Markdown source={getResumeFromContent(post.content, false, 450)} />
-                </div>
+                <PostCard title={post.title} source={getResumeFromContent(post.content, true, true, 450)}/>
             )
         }        
         </div>
