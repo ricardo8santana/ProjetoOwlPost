08742fc8a401e273c1437e8fdebcb585f01b387e
BlackKing72 <matheus.csilv14@gmail.com>
2024-08-23 09:57:51 -0300

    Adicionando a página home e removendo do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,37 +13,14 @@ import PostTitulo from './components/PostTitulo';
 
 import PostEditorPage from './pages/PostEditorPage'
 import PostViewPage from './pages/PostViewPage'
+import PaginaHome from './pages/PaginaHome';
 
 
 
 const routes = createBrowserRouter([
   {
     path: '/',
-    element:
-    <>
-    <Navbar/>
-    <div>
-        <div className="home">
-            <CarroselHome/>
-            <div className="margem-carrosel-jogos">
-                <ul className="teste-1">
-                    <li className="carregamento-carrosel jogo1-1">
-                        <span>Jogo 1</span>
-                    </li>
-                    <li className="carregamento-carrosel jogo1-2">
-                        <span>Jogo 2</span>
-                    </li>
-                    <li className="carregamento-carrosel postagem1-1">
-                        <span>Postagem 1</span>
-                    </li>
-                    <li className="carregamento-carrosel postagem1-2">
-                        <span>Postagem 2</span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    </>
+    element: <PaginaHome />    
   },
   {
     path: '/post',
