a0801f08fac2f26017fe888d23e8eb581478e3de
BlackKing72 <matheus.csilv14@gmail.com>
2024-08-27 19:10:39 -0300

    Removendo um usuário invalido dos posts de teste

--- src/services/postService.jsx
+++ src/services/postService.jsx
@@ -12,15 +12,15 @@ export class Post {
 }
 
 const posts = [
-    new Post(0, new Date(), [ getTags()[0] ], 'Markdown 01', 'Lorem markdownum, nocebit montibus viscera, erat, mihi sisto, manet. Dura fratres, balatus pater: *Cypriae* nunc collo ora mollibat metuam si memores occiderat, pyropo.'), 
-    new Post(1, new Date(), [ getTags()[1] ], 'Markdown 02', 'Sagax [contingere soceri](#propior-sed). **Fuerant** barbaricoque habent condiderant non conspecta [celatos deterruit ut](#non) stabat custos vocari flamma olivae sed parvoque nymphae o atris?'),
-    new Post(2, new Date(), [ getTags()[2] ], 'Markdown 03', 'Astu velit, despectabat terris optima [trepidare](#et-namque-turbine) ferox feres Lucifero sine foribus sanguisque, mare est et levi succedere, mane? Timendi prope, valent nec pereat curvamine **procis**, et.'),
     new Post(1, new Date(), [ getTags()[0] ], 'Markdown 01', 'Lorem markdownum, nocebit montibus viscera, erat, mihi sisto, manet. Dura fratres, balatus pater: *Cypriae* nunc collo ora mollibat metuam si memores occiderat, pyropo.'), 
     new Post(2, new Date(), [ getTags()[1] ], 'Markdown 02', 'Sagax [contingere soceri](#propior-sed). **Fuerant** barbaricoque habent condiderant non conspecta [celatos deterruit ut](#non) stabat custos vocari flamma olivae sed parvoque nymphae o atris?'),
-    new Post(0, new Date(), [ getTags()[2] ], 'Markdown 03', 'Astu velit, despectabat terris optima [trepidare](#et-namque-turbine) ferox feres Lucifero sine foribus sanguisque, mare est et levi succedere, mane? Timendi prope, valent nec pereat curvamine **procis**, et.'),
-    new Post(2, new Date(), [ getTags()[0] ], 'Markdown 01', 'Lorem markdownum, nocebit montibus viscera, erat, mihi sisto, manet. Dura fratres, balatus pater: *Cypriae* nunc collo ora mollibat metuam si memores occiderat, pyropo.'), 
-    new Post(0, new Date(), [ getTags()[1] ], 'Markdown 02', 'Sagax [contingere soceri](#propior-sed). **Fuerant** barbaricoque habent condiderant non conspecta [celatos deterruit ut](#non) stabat custos vocari flamma olivae sed parvoque nymphae o atris?'),
+    new Post(3, new Date(), [ getTags()[2] ], 'Markdown 03', 'Astu velit, despectabat terris optima [trepidare](#et-namque-turbine) ferox feres Lucifero sine foribus sanguisque, mare est et levi succedere, mane? Timendi prope, valent nec pereat curvamine **procis**, et.'),
+    new Post(3, new Date(), [ getTags()[0] ], 'Markdown 01', 'Lorem markdownum, nocebit montibus viscera, erat, mihi sisto, manet. Dura fratres, balatus pater: *Cypriae* nunc collo ora mollibat metuam si memores occiderat, pyropo.'), 
+    new Post(2, new Date(), [ getTags()[1] ], 'Markdown 02', 'Sagax [contingere soceri](#propior-sed). **Fuerant** barbaricoque habent condiderant non conspecta [celatos deterruit ut](#non) stabat custos vocari flamma olivae sed parvoque nymphae o atris?'),
     new Post(1, new Date(), [ getTags()[2] ], 'Markdown 03', 'Astu velit, despectabat terris optima [trepidare](#et-namque-turbine) ferox feres Lucifero sine foribus sanguisque, mare est et levi succedere, mane? Timendi prope, valent nec pereat curvamine **procis**, et.'),
+    new Post(1, new Date(), [ getTags()[0] ], 'Markdown 01', 'Lorem markdownum, nocebit montibus viscera, erat, mihi sisto, manet. Dura fratres, balatus pater: *Cypriae* nunc collo ora mollibat metuam si memores occiderat, pyropo.'), 
+    new Post(3, new Date(), [ getTags()[1] ], 'Markdown 02', 'Sagax [contingere soceri](#propior-sed). **Fuerant** barbaricoque habent condiderant non conspecta [celatos deterruit ut](#non) stabat custos vocari flamma olivae sed parvoque nymphae o atris?'),
+    new Post(2, new Date(), [ getTags()[2] ], 'Markdown 03', 'Astu velit, despectabat terris optima [trepidare](#et-namque-turbine) ferox feres Lucifero sine foribus sanguisque, mare est et levi succedere, mane? Timendi prope, valent nec pereat curvamine **procis**, et.'),
 ];
 
 export const getPosts = () => {
