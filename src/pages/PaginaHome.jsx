a60c05f308c72d6347977c1078e397759797d28e
BlackKing72 <matheus.csilv14@gmail.com>
2024-09-03 15:20:18 -0300

    Consertando o bug do carousel sobrepor o navbar

--- src/pages/PaginaHome.jsx
+++ src/pages/PaginaHome.jsx
@@ -54,39 +54,23 @@ const PaginaHome = () => {
 						</div>
 					</div>
 				</div>
-				{/* <div className="home">
-					<CarroselHome />
-					<div className="margem-carrosel-jogos">
-						<ul className="teste-1">
-							<li className="carregamento-carrosel jogo1-1">
-								<span>Jogo 1</span>
-							</li>
-							<li className="carregamento-carrosel jogo1-2">
-								<span>Jogo 2</span>
-							</li>
-							<li className="carregamento-carrosel postagem1-1">
-								<span>Postagem 1</span>
-							</li>
-							<li className="carregamento-carrosel postagem1-2">
-								<span>Postagem 2</span>
-							</li>
-						</ul>
-					</div>
-				</div> */}
+				
 			</PageSection>
-			<PageSection variant='secondary' isEnd>
+			<PageSection variant='secondary'>
 				<div className="sobre">
 					<h3>Um pouco sobre o site</h3>
-
 
 					<p>No início, não tínhamos uma ideia clara do que criar. No entanto, surgiu a oportunidade de desenvolver algo
 						que pudesse ajudar a turma de enfermagem a entender melhor o conteúdo. Com isso em mente, decidimos criar um jogo. Estamos empenhados
 						em desenvolver um jogo que torne o aprendizado de conteúdos complexos mais fácil e divertido.</p>
 					<p>Além do jogo criamos um site ,com algumas telas como por exemplo a tela de login, perfil do usuário e a tela de conquista. </p>
-
+				</div>
+				</PageSection>
+				<PageSection isEnd >
+					<div>
 					<h3>Os integrantes do grupo </h3>
 					<p>Vinicius Lima Campos</p> <p>Jonatas Tavares Pepolin </p> <p>Michaell Senna Amaral Cordeiro</p> <p>Luis Ricardo De Santana </p> <p>Gustavo Dos Santos Magalhães.</p> <p>Matheus Cruz</p>
-				</div>
+					</div>
 			</PageSection>
 		</>
 	)
