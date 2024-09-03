b122538229e96aa9782bc73a310f07306da558b1
BlackKing72 <matheus.csilv14@gmail.com>
2024-09-03 19:34:08 -0300

    Finalizando a base da página home

--- src/pages/PaginaHome.jsx
+++ src/pages/PaginaHome.jsx
@@ -1,6 +1,8 @@
 import CarroselHome from "../components/Carrosel";
 import Navbar from "../components/Navbar";
 import PageSection from "../components/PageSection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube, faXTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 import './PaginaHome.css';
 
@@ -28,51 +30,122 @@ const slides = [
 
 const PaginaHome = () => {
 	return (
-		<>
+		<div className="home">
 			<Navbar />
 			<PageSection isStart>
-				<div className="home-carousel">
-					<div className="home-carousel-container">
-						<CarroselHome slides={slides} />
-					</div>
-					<div className="home-content-container">
-						<div className="content-card">
-							<h1>Título</h1>
-							<span>Descrição</span>
+				<div className="destaques">
+					<h1>Destaques</h1>
+					<hr />
+					<div className="home-carousel">
+						<div className="home-carousel-container">
+							<CarroselHome slides={slides} />
 						</div>
-						<div className="content-card">
-							<h1>Título</h1>
-							<span>Descrição</span>
-						</div>
-						<div className="content-card">
-							<h1>Título</h1>
-							<span>Descrição</span>
-						</div>
-						<div className="content-card">
-							<h1>Título</h1>
-							<span>Descrição</span>
+						<div className="home-content-container">
+							<div className="content-card">
+								<h1>Título</h1>
+								<span>Descrição</span>
+							</div>
+							<div className="content-card">
+								<h1>Título</h1>
+								<span>Descrição</span>
+							</div>
+							<div className="content-card">
+								<h1>Título</h1>
+								<span>Descrição</span>
+							</div>
+							<div className="content-card">
+								<h1>Título</h1>
+								<span>Descrição</span>
+							</div>
 						</div>
 					</div>
 				</div>
-				
+
 			</PageSection>
 			<PageSection variant='secondary'>
 				<div className="sobre">
-					<h3>Um pouco sobre o site</h3>
+					<h1>Um pouco sobre o site</h1>
+					<hr/>
 
-					<p>No início, não tínhamos uma ideia clara do que criar. No entanto, surgiu a oportunidade de desenvolver algo
-						que pudesse ajudar a turma de enfermagem a entender melhor o conteúdo. Com isso em mente, decidimos criar um jogo. Estamos empenhados
-						em desenvolver um jogo que torne o aprendizado de conteúdos complexos mais fácil e divertido.</p>
-					<p>Além do jogo criamos um site ,com algumas telas como por exemplo a tela de login, perfil do usuário e a tela de conquista. </p>
-				</div>
-				</PageSection>
-				<PageSection isEnd >
-					<div>
-					<h3>Os integrantes do grupo </h3>
-					<p>Vinicius Lima Campos</p> <p>Jonatas Tavares Pepolin </p> <p>Michaell Senna Amaral Cordeiro</p> <p>Luis Ricardo De Santana </p> <p>Gustavo Dos Santos Magalhães.</p> <p>Matheus Cruz</p>
+					<div className="sobre-buble">
+						<h5>Como surgiu a ideia da gamificação?</h5>
+						<p>No início, não tínhamos uma ideia clara do que criar. No entanto, surgiu a oportunidade de desenvolver algo
+							que pudesse ajudar a turma de enfermagem. Com isso em mente, decidimos criar um jogo. Usando a ideia da
+							gamificação, estamos empenhados em desenvolver um jogo que torne o aprendizado de conteúdos complexos mais fácil e divertido.</p>
 					</div>
+
+					<div className="sobre-buble">
+						<h5>Como surgiu a ideia da plataforma?</h5>
+						<p>Como não seria possível criar um jogo que cobriria a quantidade de conteúdo da turma de enfermagem, começamos a pensar
+							em outras maneiras de fazer isso. Foi assim que surgiu a ideia trazer todo esse conteúdo para um único lugar. Professores
+							e alunos compartilham conteúdos que eles conheçam e que estariam espalhados em livros, ou sites e outros alunos poderam
+							acessar esse conteúdo.</p>
+					</div>
+				</div>
 			</PageSection>
-		</>
+			<PageSection>
+				<div className="sobre">
+					<h1>Os integrantes do grupo </h1>
+					<hr />
+
+					<div className="integrantes">
+						<div className="integrante">
+							<div className="foto-perfil">
+								{/* https://github.com/{username}.png?size=40 */}
+								<img src="https://avatars.githubusercontent.com/u/172276584?s=250&v=4" />
+							</div>
+							<p>Vinicius Lima Campos</p>
+						</div>
+						<div className="integrante">
+							<div className="foto-perfil">
+								<img src="https://avatars.githubusercontent.com/u/165349828?s=250&v=4" />
+							</div>
+							<p>Jonatas Tavares Pepolin</p>
+						</div>
+						<div className="integrante">
+							<div className="foto-perfil">
+								<img src="https://avatars.githubusercontent.com/u/172276017?s=250&v=4" />
+							</div>
+							<p>Michaell Senna Amaral Cordeiro</p>
+						</div>
+						<div className="integrante">
+							<div className="foto-perfil">
+								<img src="https://avatars.githubusercontent.com/u/51215442?s=250&v=4" />
+							</div>
+							<p>Luis Ricardo De Santana</p>
+						</div>
+						<div className="integrante">
+							<div className="foto-perfil">
+								<img src="https://avatars.githubusercontent.com/u/172276584?s=250&v=4" />
+							</div>
+							<p>Gustavo Dos Santos Magalhães</p>
+						</div>
+						<div className="integrante">
+							<div className="foto-perfil">
+								<img src="https://avatars.githubusercontent.com/u/66685044?s=250&v=4" />
+							</div>
+							<p>Matheus Cruz da Silva</p>
+						</div>
+					</div>
+					     
+				</div>
+			</PageSection>
+			<PageSection isEnd hugContent variant='alternative'>
+				<div className="enquadroFooter">
+					<div className="alinhamento-icones">
+						<FontAwesomeIcon icon={faYoutube} />
+						<FontAwesomeIcon icon={faXTwitter} />
+						<FontAwesomeIcon icon={faFacebook} />
+					</div>
+					<hr/>
+					<p>&copy; 2024, OwlPost,Inc. Todos os direitos reservados.</p>
+					<div className="alinhamento-links">
+						<a href="#">Termos de Serviço</a>
+						<a href="#">Politica de Privacidade</a>
+					</div>
+				</div>
+			</PageSection>
+		</div>
 	)
 };
 
