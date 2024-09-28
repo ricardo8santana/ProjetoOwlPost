cde2b5b67bde46a49b6060a7fa727e2da3b65837
BlackKing72 <matheus.csilv14@gmail.com>
2024-09-28 09:14:54 -0300

    Removendo o código antigo

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -6,38 +6,13 @@ import './DarkModeToggle.css'
 
 import * as darkModeService from '../services/darkModeService';
 
-const ThemeStorageKey = 'theme';
-const ThemeDark = 'dark';
-const ThemeLight = 'light';
-
 const DarkModeToggle = () => {
     const [useDarkMode, setUseDarkMode] = useState(false);
-
-    // const setTheme = (prefersDarkMode) => {
-    //     setUseDarkMode(prefersDarkMode);
-
-    //     if (prefersDarkMode) {
-    //         localStorage.setItem(ThemeStorageKey, ThemeDark);
-    //         document.body.classList.add('dark-mode');
-    //     }
-    //     else {
-    //         localStorage.setItem(ThemeStorageKey, ThemeLight);
-    //         document.body.classList.remove('dark-mode');
-    //     }
-    // };
 
     useEffect(() => {
         const prefersDarkMode = darkModeService.loadTheme();
         darkModeService.setTheme(prefersDarkMode);
         setUseDarkMode(prefersDarkMode);
-
-        // const currentTheme = localStorage.getItem(ThemeStorageKey);
-
-        // let prefersDarkMode = currentTheme !== null
-        //     ? currentTheme === ThemeDark
-        //     : window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-        // setTheme(prefersDarkMode);
     }, []);
 
     return (
